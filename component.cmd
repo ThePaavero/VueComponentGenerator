e3d39c3b39e6453f8d98cd983f900eaa44a7426d
ThePaavero <pekka@astudios.org>
2017-08-24 21:31:21 +0300

    doc

--- component.cmd
+++ component.cmd
@@ -1,2 +1,2 @@
-REM Edit this path to reflect your setup and move this file to within your PATH.
+REM Edit the path below to reflect your setup and move this file to within your PATH.
 @node "D:\node\vuecomponentgenerator\vuecomponentgenerator.js" %*
