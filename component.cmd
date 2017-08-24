dfeb20c381b3b6fe3c59639477323511fbf45540
ThePaavero <pekka@astudios.org>
2017-08-24 21:30:58 +0300

    Re-added SCSS

--- component.cmd
+++ component.cmd
@@ -1,2 +1,2 @@
-REM Edit this path to reflect your setup.
+REM Edit this path to reflect your setup and move this file to within your PATH.
 @node "D:\node\vuecomponentgenerator\vuecomponentgenerator.js" %*
